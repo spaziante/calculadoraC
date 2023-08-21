4d226e0f2dd7887cd4189c093ea4ef1970fe99aa
spaziante <gabrielfspaziante7@gmail.com>
2023-08-21 04:16:39 -0300

    funcao subtracao

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <stdbool.h>
+
+void subtracao(float *numero1, float *numero2, float *resultado){
+    printf("Digite  primeiro numero da subtração: ");
+    scanf("%f", numero1);
+    printf("\nDigite o segundo numero da subtração: ");
+    scanf("%f", numero2);
+
+    *resultado = *numero1 - *numero2;
+
+    }
