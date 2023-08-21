1923f5921f86999d0ea65b81f8c7d78161e7b069
spaziante <gabrielfspaziante7@gmail.com>
2023-08-21 07:23:17 -0300

    subtracao

--- main.c
+++ main.c
@@ -15,3 +15,46 @@ void subtracao(float *numero1, float *numero2, float *resultado){
     sleep(1);
 
     }
+
+    int main()
+{
+    bool loop = true;
+    char escolha;
+
+    float resultado;
+    float numero1;
+    float numero2;
+
+    while (loop == true)
+    {
+        printf("Escolha a operacao que voce deseja fazer: \n");
+        printf("Soma ( + ) \n");
+        printf("Subtracao ( - ) \n");
+        printf("Multiplacao ( * ) \n");
+        printf("Divisao ( / ) \n");
+        printf("Fechar Programa = F \n");
+        
+        scanf(" %c", &escolha);
+
+        if (escolha == 'f' || escolha == 'F')
+        {
+            loop = false;
+            printf("Saindo....");
+            sleep(2);
+            return 0;
+        } else if (escolha == '+'){
+            soma(&numero1, &numero2, &resultado);
+        } else if (escolha == '/') {
+            divisao(&numero1, &numero2, &resultado);
+        } 
+          else if (escolha == '-') {
+            subtracao(&numero1, &numero2, &resultado);
+        } 
+          else if (escolha == '*') {
+            multiplicacao(&numero1, &numero2, &resultado);
+        } 
+    
+    }
+
+    return 0;
+}
