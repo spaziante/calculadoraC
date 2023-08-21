bc69fee3b43065180b845211d3415645e66acb13
spaziante <gabrielfspaziante7@gmail.com>
2023-08-21 04:18:42 -0300

    funcao multiplicacao

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <stdbool.h>
+
+void multiplicacao(float *numero1, float *numero2, float *resultado){
+    printf("Digite  primeiro numero da multiplicação: ");
+    scanf("%f", numero1);
+    printf("\nDigite o segundo numero da multiplicação: ");
+    scanf("%f", numero2);
+
+    *resultado = *numero1 * *numero2;
+
+    }
